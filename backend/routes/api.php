a3982994609e51d8e9a57abfcbf95ed956cc8d7a
vipinrajkv <vipinrajsree@gmail.com>
2025-04-01 23:34:45 +0530

    Authentication added in backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('products', ProductController::class);
+Route::post('/register',[RegisterController::class, 'registerUser'])->name('register');
+Route::post('/login', [RegisterController::class, 'login']);
+Route::post('/logout', [RegisterController::class, 'logout']);
